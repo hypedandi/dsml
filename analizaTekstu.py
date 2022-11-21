37a8494ab7c3b452904f2a7e7b824df004a6612c
dan <108793930+hypedandi@users.noreply.github.com>
2022-11-21 10:45:05 +0100

    Update analizaTekstu.py

--- analizaTekstu.py
+++ analizaTekstu.py
@@ -1,13 +1,16 @@
 !pip install nltk
 
-!pip install spacy
-!python -m spacy download en
+# tekst do analizy
 
 paragraph="""Taj Mahal is one of the beautiful monuments. It is one of the
 wonders of the world. It was built by Shah Jahan in 1631 in memory of his
 third beloved wife Mumtaj Mahal."""
 
+# zmiana liter na małe w całym akapicie
+
 print(paragraph.lower())
+
+# Tokenizacja
 
 import nltk
 
@@ -16,7 +19,38 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 
+# tokenizacja za pomocą metody sent_tokenize
+
 from nltk.tokenize import sent_tokenize
 
-tokenized_sentences=sent_tokenize(paragraph)
+tokenized_sentences = sent_tokenize(paragraph)
 print(tokenized_sentences)
+
+# STOPWORDS usuwanie słów nieinformatywnych przy pomocy NLTK
+
+from nltk.corpus import stopwords
+stopwords_set=set(stopwords.words("english"))
+
+filtered_word_list=[]
+
+
+for word in tokenized_words:
+    if word not in stopwords_set:
+       filtered_word_list.append(word)
+       
+print("Lista tokenizowanych słów:", tokenized_words)
+print("Lista przefiltrowanych słów:", filtered_word_list)
+
+# PoS tagging
+
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+
+sentence = "Taj Mahal is one of the beautiful monument."
+sent_tokens = word_tokenize(sentence)
+sent_pos = pos_tag(sent_tokens)
+print(sent_pos)
+
+
+
+
